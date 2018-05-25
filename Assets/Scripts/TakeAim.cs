418361f8a43779696178e185ac0fe8cd74153e3e
acheddadi <alicheddadi@gmail.com>
2018-05-25 18:44:44 -0400

    Perfected Shooting Mecanim
    Tight, tight tight tight!

--- Assets/Scripts/TakeAim.cs
+++ Assets/Scripts/TakeAim.cs
@@ -8,10 +8,10 @@ public class TakeAim : MonoBehaviour
 	private Animator anime;
 	private CameraController camRot;
 	private Camera camObj;
-	private float initZoom;
-    private float velocity = 0.0f, smoothTime = 0.1f;
-	private float lastShot = 0.0f;
-	private bool recoiling = false;
+	private AudioSource sfx;
+	private float initZoom, velocity = 0.0f, smoothTime = 0.1f, lastShot = 0.0f;
+	private bool recoiling = false, letGo = true;
+	private int lastClick;
 
 	// Use this for initialization
 	void Start ()
@@ -19,6 +19,7 @@ public class TakeAim : MonoBehaviour
 		anime = GetComponent<Animator>();
 		camRot = GetComponent<CameraController>();
 		camObj = FindObjectOfType<Camera>();
+		sfx = GetComponent<AudioSource>();
 		initZoom = camObj.fieldOfView;
 	}
 	
@@ -27,16 +28,27 @@ public class TakeAim : MonoBehaviour
 	{
 		if (!GameController.gameIsPaused)
 		{
+			if (Input.GetMouseButtonDown(0)) lastClick = 0;
+			if (Input.GetMouseButtonDown(1)) lastClick = 1;
 			if (Input.GetMouseButton(1))
 			{
 				anime.SetBool("RightClick", true);
 				camRot.FPS(true);
 				camObj.fieldOfView = Mathf.SmoothDamp(camObj.fieldOfView, initZoom - 20, ref velocity, smoothTime);
-				if (Input.GetMouseButtonDown(0) && (lastShot > fireRate))
+				
+				if (Input.GetMouseButton(0) && (lastShot > fireRate) && letGo)
 				{
-					anime.SetTrigger("LeftClick");
-					StartCoroutine(RecoilCamera());
-					lastShot = 0.0f;
+					AnimatorStateInfo info = anime.GetCurrentAnimatorStateInfo(0);
+					AnimatorStateInfo nextInfo = anime.GetNextAnimatorStateInfo(0);
+					if ((info.normalizedTime > 1.0f) && (nextInfo.normalizedTime == 0.0f) && (info.IsName("Base.PointGun")) && (lastClick == 0))
+					{
+						anime.SetTrigger("LeftClick");
+						StartCoroutine(RecoilCamera());
+						sfx.pitch = Random.Range(0.9f, 1.1f);
+						sfx.Play();
+						lastShot = 0.0f;
+						letGo = false;
+					}
 				}
 			}
 			else
@@ -45,6 +57,7 @@ public class TakeAim : MonoBehaviour
 				camRot.FPS(false);
 				camObj.fieldOfView = Mathf.SmoothDamp(camObj.fieldOfView, initZoom, ref velocity, smoothTime);
 			}
+			if (Input.GetMouseButtonUp(0)) letGo = true;
 			lastShot += Time.deltaTime;
 		}
 	}
