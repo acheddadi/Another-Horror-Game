167ffe7f07a253f75ecf5827ac36371543dc5ca6
acheddadi <alicheddadi@gmail.com>
2018-05-30 19:54:31 -0400

    Tweaking lighting

---
+++ Assets/Scripts/Engine/FogController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour
+{
+	private PlayerController player;
+	// Use this for initialization
+	void Start ()
+	{
+		player = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (player != null)
+		{
+			transform.position = player.transform.position;
+		}
+	}
+}
