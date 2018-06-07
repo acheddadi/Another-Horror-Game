b1cc75e7e842ef5c997eefa0d5e4c2e7d90151a4
acheddadi <alicheddadi@gmail.com>
2018-06-06 22:54:08 -0400

    Enabled collab, health system and level 1 wip

---
+++ Assets/Scripts/Player/DamageWobble.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageWobble : MonoBehaviour
+{
+	[SerializeField]private float wobbleStrength = 5.0f;
+	private const int MULTIPLIER = 100;
+	private bool isWobbling = false;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) StartCoroutine(Wobble());
+	}
+
+	private IEnumerator Wobble()
+	{
+		if (!isWobbling)
+		{
+			isWobbling = true;
+			Vector3 initRot = transform.localEulerAngles;
+			for (float i = 0.0f; i < 1.0f; i += 0.1f)
+			{
+				transform.localEulerAngles = initRot + Vector3.forward * i * wobbleStrength * MULTIPLIER * Time.deltaTime;
+				yield return null;
+			}
+			transform.localEulerAngles = initRot + Vector3.forward * wobbleStrength;
+			for (float i = -1.0f; i < 0.0f; i += 0.1f)
+			{
+				transform.localEulerAngles = initRot + Vector3.forward * i * wobbleStrength * MULTIPLIER * Time.deltaTime;
+				yield return null;
+			}
+			transform.localEulerAngles = initRot;
+			isWobbling = false;
+		}
+	}
+}
---
+++ Assets/Scripts/UI/BloodOverlay.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloodOverlay : MonoBehaviour
+{
+	[SerializeField]private Texture[] bloodTexture;
+	private RawImage img;
+	private Texture originalTexture;
+	private int index = 0, count = 0;
+	private float increments;
+	private bool direction = true;
+
+	void Start()
+	{
+		img = GetComponent<RawImage>();
+		increments = 100 / bloodTexture.Length;
+	}
+
+	void Update()
+	{
+		if (direction)
+		{
+			if (img.color.a > 0.5f)
+			{
+				float alpha = Mathf.Clamp(img.color.a - Time.deltaTime, 0.5f, 1.0f);
+				img.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+			}
+			else direction = !direction;
+		}
+		else
+		{
+			if (img.color.a < 1.0f)
+			{
+				float alpha = Mathf.Clamp(img.color.a + Time.deltaTime, 0.5f, 1.0f);
+				img.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+			}
+			else direction = !direction;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log(count);
+			switch (count)
+			{
+				case 0:
+				NextTexture(100);
+				break;
+				case 1:
+				NextTexture(90);
+				break;
+				case 2:
+				NextTexture(80);
+				break;
+				case 3:
+				NextTexture(70);
+				break;
+				case 4:
+				NextTexture(60);
+				break;
+				case 5:
+				NextTexture(50);
+				break;
+				case 6:
+				NextTexture(40);
+				break;
+				case 7:
+				NextTexture(30);
+				break;
+				case 8:
+				NextTexture(20);
+				break;
+				case 9:
+				NextTexture(10);
+				break;
+				case 10:
+				NextTexture(0);
+				break;
+			}
+			if (count > 9) count = 0;
+			else count++;
+		}
+	}
+
+	void OnDestroy()
+	{
+		img.texture = originalTexture;
+	}
+
+	void NextTexture(int health)
+	{
+		int damage = 100 - health;
+		index = Mathf.Clamp(Mathf.RoundToInt(damage / increments) - 1, 0, bloodTexture.Length - 1);
+		img.texture = bloodTexture[index];
+	}
+}
