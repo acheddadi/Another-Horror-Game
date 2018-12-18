4394ca9e67f163bce2ba4c311489b9581c33b2a7
acheddadi <alicheddadi@gmail.com>
2018-12-18 00:21:54 -0500

    Started Inventory
    Figuring things out at the speed of slow.

---
+++ Assets/Scripts/UI/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    [SerializeField] private bool canEquip;
+    [SerializeField]private Sprite itemSprite;
+    [TextArea(3, 10)]
+    [SerializeField]private string[] description;
+}
