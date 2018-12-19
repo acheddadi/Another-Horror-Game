51b0a1f291bc8f08cad5c3681e13827b56838def
acheddadi <alicheddadi@gmail.com>
2018-12-19 01:57:26 -0500

    UI work, nothing much.

--- Assets/Scripts/UI/Item.cs
+++ Assets/Scripts/UI/Item.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Item
 {
-    [SerializeField] private bool canEquip;
+    [SerializeField]private bool canEquip;
     [SerializeField]private Sprite itemSprite;
     [TextArea(3, 10)]
     [SerializeField]private string[] description;
